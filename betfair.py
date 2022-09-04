17eba07b05c31a560bbe43b0fdb9e60f6cd8aeac
stefarusso <rush.steve95@gmail.com>
2022-09-04 15:40:32 +0200

    basic login implementation

--- betfair.py
+++ betfair.py
@@ -16,8 +16,18 @@ def get_ssoid(usr,psw,ap_key,certificates,url):
     else:
         print('Houston we have a problem!!!')
         print('login status betfair API = ' + resp_json['loginStatus'])
+        print('all the return codes are available here https://docs.developer.betfair.com/display/1smk3cen4v3lu3yomq5qye0ni/Non-Interactive+%28bot%29+login')
     return response['sessionToken']
 
+#GET THE SESSION TOKEN
 non_interactive_login_url='https://identitysso-cert.betfair.it/api/certlogin'    #Just for get the session token SSOID on the italian website
 certificates=('/media/2TB/oddsmatcher/certificate_xca/betfair_api.crt','/media/2TB/oddsmatcher/certificate_xca/betfair_api.key') #local position of my XRC certificates and secret key
-print(get_ssoid(usr,psw,ap_key,certificates,non_interactive_login_url))
+ssoid=get_ssoid(usr,psw,ap_key,certificates,non_interactive_login_url)
+
+#
+betting_api_url='https://api.betfair.com/exchange/betting/json-rpc/v1'
+headers={'X-Application': ap_key, 'X-Authentication' : ssoid ,'content-type' : 'application/json'}
+json_req='{"jsonrpc": "2.0", "method": "SportsAPING/v1.0/listEventTypes", "params": {"filter":{ }}, "id": 1}'
+response = requests.post(betting_api_url, data=json_req.encode('utf-8'), headers=headers)
+response = (response)
+print(response)
