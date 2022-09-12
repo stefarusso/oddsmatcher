854b13f77397906e6cf85fef2ac03440c4805fcb
stefarusso <stefarusso@rush.steve95@gmail.com>
2022-09-12 17:41:58 +0200

    finished all functions

--- betfair.py
+++ betfair.py
@@ -5,12 +5,14 @@ import scraperPS as sps
 import datetime
 import betfairlightweight
 import pandas as pd
-pd.set_option('display.max_columns', None) #FOR DEBUG
-pd.set_option('display.max_row', None) #FOR DEBUG
+
 
 
 
 #USEFULL ONLY IN DEBUGGING----------------------------------------------------------------------------------------------------------------------------------------
+#pd.set_option('display.max_columns', None) #FOR DEBUG
+#pd.set_option('display.max_row', None) #FOR DEBUG
+
 def get_ssoid(usr,psw,ap_key,certificates,url):
     payload = 'username=' + usr + '&password=' + psw
     headers = {'X-Application': ap_key, 'Content-Type': 'application/x-www-form-urlencoded'}
@@ -26,10 +28,10 @@ def get_ssoid(usr,psw,ap_key,certificates,url):
 
 
 #GET THE SESSION TOKEN
-non_interactive_login_url='https://identitysso-cert.betfair.it/api/certlogin'    #Just for get the session token SSOID on the italian website
-certificates=('certificates/betfair_api.crt','certificates/betfair_api.key') #local position of my XRC certificates and secret key
-ssoid=get_ssoid(usr,psw,ap_key,certificates,non_interactive_login_url)
-print("ssoid:" + ssoid)
+#non_interactive_login_url='https://identitysso-cert.betfair.it/api/certlogin'    #Just for get the session token SSOID on the italian website
+#certificates=('certificates/betfair_api.crt','certificates/betfair_api.key') #local position of my XRC certificates and secret key
+#ssoid=get_ssoid(usr,psw,ap_key,certificates,non_interactive_login_url)
+#print("ssoid:" + ssoid)
 #USEFULL ONLY IN DEBUGGING-------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -45,6 +47,15 @@ def open_json_file(filename):
 def save_json_file(filename,object):
 	with open(filename, 'w') as outfile:
 		json.dump(object, outfile,indent=2)
+
+def init_data(col_names = ['league', 'home_team', 'away_team', 'date', 'selection', 'lay_price','lay_size']):
+    data = pd.DataFrame(columns=col_names)  # DATAFRAME FOR 1x2
+    return data
+
+def list_to_dataframe(List,col_names=['league', 'home_team', 'away_team', 'date', 'selection', 'lay_price','lay_size']):
+    List=dict(zip(col_names,List))
+    List=pd.DataFrame([List])
+    return List
 
 #FUNTION USED BY initialize_competition_dict()
 #CHECK WOMAN (F) F=Femminile COMPETITIONS
@@ -81,7 +92,7 @@ def initialize_competition_dict(filename,competitions_pokerstar, soccer_id,datet
     return competition_dict
 
 #API REQUEST OF THE COMPETITIONS ID. LIKE SERIE A , CHAMPIONS LEAUGE...
-def request_event_list(soccer_id,competition_id):
+def request_event_list(trading,soccer_id,competition_id):
     event_filter = betfairlightweight.filters.market_filter(event_type_ids=soccer_id,competition_ids=competition_id,market_start_time={'to': (datetime.datetime.utcnow() + datetime.timedelta(weeks=1)).strftime("%Y-%m-%dT%TZ")})
     events=trading.betting.list_events(filter=event_filter)
     return events
@@ -104,7 +115,7 @@ def extract_event(event_list):
     return event_l
 
 #FUNCTION WHO EXCTRACT THE MARKET IDS FOR THE EVENT WE WONT
-def extract_market_catalogue(event_id):
+def extract_market_catalogue(trading,event_id):
     markets_set = ["MATCH_ODDS", "OVER_UNDER_25", "BOTH_TEAMS_TO_SCORE"]
     betfair_to_pokerstars = {'Over 2,5 goal': 'Over 2.5', 'Under 2,5 goal': 'Under 2.5', 'Sì': 'GOAL', 'No': 'NOGOAL'}
     market_catalogue_filter = betfairlightweight.filters.market_filter(event_ids=event_id,market_type_codes=markets_set)
@@ -130,7 +141,7 @@ def extract_market_catalogue(event_id):
     return selection_dict, market_dict
 
 #API REQUEST FOR MARKET BOOKS WITH LAY PRICE AND LAY SIZE FOR ALL THE MARKET IDs
-def request_market_book(market_ids):
+def request_market_book(trading,market_ids):
     price_filter = betfairlightweight.filters.price_projection(price_data=['EX_BEST_OFFERS'])
     market_books = trading.betting.list_market_book(market_ids=market_ids,price_projection=price_filter)
     return market_books
@@ -148,79 +159,91 @@ def extract_runner_lay(runner_book,market_id,market_dict,selection_dict):
     except IndexError:
         price="NA"
         size="NA"
-    return comp,home, away,date,selection_name, price, size
+    #return comp,home, away,date,selection_name, price, size
+    return [comp , home , away , date , selection_name , price , size]
 
 #DOWNLOAD THE FILE WITH ALL THE MARKET ID WE HAVE. 40 ENTRY AT TIME BECAUSE BETFAIR API HAS MAX DATA IN THE REQUESTS
-def export_market_book(market_dict):
+def export_market_book(trading,market_dict):
     market_ids = list(market_dict.keys())
     market_len = len(market_ids)
     market_books = []
     if market_len > 40:
         start = 0
         stop = 40
-        market_books.extend(request_market_book(market_ids[start:stop]))
+        market_books.extend(request_market_book(trading,market_ids[start:stop]))
         for i in range(market_len // 40 - 1):
             start += 40
             stop += 40
-            market_books.extend(request_market_book(market_ids[start:stop]))
+            market_books.extend(request_market_book(trading,market_ids[start:stop]))
         modulus = market_len % 40
         if modulus != 0:
             start += 40
             stop += market_len % 40
-            market_books.extend(request_market_book(market_ids[start:stop]))
+            market_books.extend(request_market_book(trading,market_ids[start:stop]))
     return market_books
 
 #FINAL FUNCTION WHICH EXPORT FINAL DATAFRAME
 def export_runners(market_books,market_dict,selection_dict):
+    export_dataframe=init_data()
     for market in market_books:
         market_id=str(market.market_id)
-        export_list=[]
+        #export_list=[]
         #print(market_dict["1.203229165"])
         #print(json.dumps(json.loads(market.json()), indent=2))
         for runner in market.runners:
             runner_lay=extract_runner_lay(runner,market_id,market_dict,selection_dict)
-            print(runner_lay) #change in appen row on a pandas dataframe
-            export_list.append(runner_lay)
-    return export_list
+            #export_list.append(runner_lay)
+            export_dataframe = pd.concat([export_dataframe, list_to_dataframe(runner_lay)], ignore_index=True)   
+    return export_dataframe
 
 
 
-#LOGIN IN THE API
-certificate_root='oddsmatcher/certificates/' #local position of my XRC certificates and secret key
-trading = betfairlightweight.APIClient(usr, psw, app_key=ap_key, certs=certificate_root,locale="italy")
-#FOR OBVIOUS REASON MY AP_KEY AND CERTIFICATES ARE NOT UPLOADED ON GITHUB
-#THE AP_KEY CAN BE REQUESTED FOR EVERYONE WHO HAVE A VERIFIED ACCOUNT ON BETFAIR
-#THE CERTIFICATES ARE PRETTY EASY TO BEING PRODUCED
-#REFERENCE MATERIAL IS WELL DESCRIBED
-#https://docs.developer.betfair.com/display/1smk3cen4v3lu3yomq5qye0ni/Getting+Started
-trading.login()
+def load_dataframe(usr, psw, ap_key):
+    #LOGIN IN THE API
+    certificate_root='oddsmatcher/certificates/' #local position of my XRC certificates and secret key
+    trading = betfairlightweight.APIClient(usr, psw, app_key=ap_key, certs=certificate_root,locale="italy")
+    
+    #FOR OBVIOUS REASON MY AP_KEY AND CERTIFICATES ARE NOT UPLOADED ON GITHUB
+    # THE AP_KEY CAN BE REQUESTED FOR EVERYONE WHO HAVE A VERIFIED ACCOUNT ON BETFAIR
+    #THE CERTIFICATES ARE PRETTY EASY TO BEING PRODUCED
+    #REFERENCE MATERIAL IS WELL DESCRIBED
+    #https://docs.developer.betfair.com/display/1smk3cen4v3lu3yomq5qye0ni/Getting+Started
+    
+    trading.login()
+    
+    #SOCCER ID
+    results  = trading.betting.list_event_types()
+    soccer_id=[result.event_type.id for result in results if result.event_type.name=="Soccer"] # Betfair API wants id in a list
+    
+    #COMPETITION ID
+    datetime_in_a_week = (datetime.datetime.utcnow() + datetime.timedelta(weeks=4)).strftime("%Y-%m-%dT%TZ")
 
-#SOCCER ID
-results  = trading.betting.list_event_types()
-soccer_id=[result.event_type.id for result in results if result.event_type.name=="Soccer"] # Betfair API wants id in a list
+    #This is given by scraperPS
+    #DA AGGIUNGERE COME ARGOMENTO DELLA FUNZIONE!!!!! DA RETURN DI SCRAPERPS
+    competitions_pokerstar=['Italia - Serie A','Italia - Serie B','Champions League','Europa League','Conference League','Inghilterra - Premier League','Spagna - La Liga','Germania - Bundesliga','Francia - Ligue 1','Portogallo - Primeira Liga']
+    competitions_pokerstar=[i.replace(" - "," ") for i in competitions_pokerstar]
+    memory_filename="competition_dict.json" #file where id competition ids are stored for minimizing the number of requests to the api
+    competition_dict=initialize_competition_dict(memory_filename,competitions_pokerstar, soccer_id,datetime_in_a_week)
+    competition_ids=dict_to_list(competition_dict,competitions_pokerstar)
 
-#COMPETITION ID
-datetime_in_a_week = (datetime.datetime.utcnow() + datetime.timedelta(weeks=4)).strftime("%Y-%m-%dT%TZ")
+    #EVENTS ID
+    events=request_event_list(trading,soccer_id,competition_ids)
+    event_id=extract_event(events)
 
-#This is given by scraperPS
-competitions_pokerstar=['Italia - Serie A','Italia - Serie B','Champions League','Europa League','Conference League','Inghilterra - Premier League','Spagna - La Liga','Germania - Bundesliga','Francia - Ligue 1','Portogallo - Primeira Liga']
-competitions_pokerstar=[i.replace(" - "," ") for i in competitions_pokerstar]
+    #MARKET ID AND SELECTION_NAMES
+    selection_dict,market_dict=extract_market_catalogue(trading,event_id)
 
-memory_filename="competition_dict.json" #file where id competition ids are stored for minimizing the number of requests to the api
-competition_dict=initialize_competition_dict(memory_filename,competitions_pokerstar, soccer_id,datetime_in_a_week)
-competition_ids=dict_to_list(competition_dict,competitions_pokerstar)
+    #MARKET BOOKS WITH THE FINAL DATA WE WANT
+    market_books=export_market_book(trading,market_dict)
+    all_runners=export_runners(market_books,market_dict,selection_dict)
+    return(all_runners)
 
 
-#EVENTS ID
-events=request_event_list(soccer_id,competition_ids)
-event_id=extract_event(events)
+if __name__ == "__main__":
+    print(load_dataframe(usr, psw, ap_key))
 
-#MARKET ID AND SELECTION_NAMES
-selection_dict,market_dict=extract_market_catalogue(event_id)
 
-#MARKET BOOKS WITH THE FINAL DATA WE WANT
-market_books=export_market_book(market_dict)
-all_runners=export_runners(market_books,market_dict,selection_dict)
+
 
 
 
